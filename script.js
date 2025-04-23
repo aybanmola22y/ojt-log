24d51e30d4841b8e95fedfc8fe23ed2255753c80
John Aivanne Molato <125982979+aybanmola22y@users.noreply.github.com>
2025-04-23 13:22:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let employees = JSON.parse(localStorage.getItem("employees")) || {
     "Yuan Andonga": { remainingHours: 600, timeIn: null, timeOut: null },
-    "Aivanne Baria": { remainingHours: 130, timeIn: null, timeOut: null },
+    "Aivanne Baria Lazarte": { remainingHours: 130, timeIn: null, timeOut: null },
     "Jayhan Molato": { remainingHours: 600, timeIn: null, timeOut: null },
     "Joseph Monroy": { remainingHours: 600, timeIn: null, timeOut: null },
     "Ma'am Claire Abalus": { remainingHours: 600, timeIn: null, timeOut: null },
